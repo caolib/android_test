8a6aee8926e7cc98e798a6a982e46d10f00ff073
clb <1265501579@qq.com>
2024-05-18 11:13:43 +0800

    修改activity跳转方式

--- test/src/main/java/io/github/caolib/MainActivity.java
+++ test/src/main/java/io/github/caolib/MainActivity.java
@@ -2,72 +2,35 @@ package io.github.caolib;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.EditText;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "clb";
     private EditText editText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "Activity-1 onCreate...");
-
         editText = findViewById(R.id.et);
 
-        findViewById(R.id.btn_commit).setOnClickListener(v -> {
-            // 获取Edittext文本内容长度
-            String s = editText.getText().length() + "字";
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-            intent.putExtra("count", s);
-            startActivity(intent);
+        ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            // 获取返回的结果
+            Intent data = result.getData();
+            if (result.getResultCode() == RESULT_OK && data != null) {
+                editText.setText(data.getStringExtra("return"));
+            }
         });
 
-        // 获取传回数据并显示
-        Intent intent = getIntent();
-        String str = intent.getStringExtra("return");
-        editText.setText(str);
+        findViewById(R.id.btn_commit).setOnClickListener(v -> {
+            // 获取EditText文本内容长度,发送给Activity2
+            String s = editText.getText().length() + "字";
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class).putExtra("count", s);
+            launcher.launch(intent);
+        });
 
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "Activity-1 onStart...");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "Activity-1 onResume...");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "Activity-1 onPause...");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "Activity-1 onDestroy...");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "Activity-1 onStop...");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "Activity-1 onRestart...");
     }
 }
--- test/src/main/java/io/github/caolib/MainActivity2.java
+++ test/src/main/java/io/github/caolib/MainActivity2.java
@@ -2,68 +2,29 @@ package io.github.caolib;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
-    public static final String TAG = "clb";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Log.d(TAG, "Activity-2 onCreate...");
-
+        // 获取activity1传递的数据并显示
         Intent intent = getIntent();
         String count = intent.getStringExtra("count");
-
         TextView textView = findViewById(R.id.tv_count);
         textView.setText(count);
 
         findViewById(R.id.tv_ok).setOnClickListener(v -> {
-            Intent i = new Intent(MainActivity2.this, MainActivity.class);
-            i.putExtra("return", "感谢您的宝贵建议！");
-            startActivity(i);
+            // 返回结果
+            Intent i = new Intent(MainActivity2.this, MainActivity.class).putExtra("return", "感谢您的宝贵建议！");
+            setResult(RESULT_OK, i);
+            finish();
         });
 
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "Activity-2 onStart...");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "Activity-2 onResume...");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "Activity-2 onPause...");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "Activity-2 onDestroy...");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "Activity-2 onStop...");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "Activity-2 onRestart...");
     }
 }
